9a7448bb1c0fd04e5b74027eee384e8e8d7eb6ab
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-04 15:55:28 +0000

    L8-Sobel added

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -12,14 +12,16 @@ gray_image = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 
 #(KernelSizeWidth, KernelSizeHeight) = 3
 #blurred image
-img3x3Blur = cv2.GaussianBlur(gray_image,(3, 3),0)
+img3x3Blur = cv2.GaussianBlur(gray_image,(3, 3),0) #3x3
 
-img13x13Blur = cv2.GaussianBlur(gray_image,(13, 13),0)
+img13x13Blur = cv2.GaussianBlur(gray_image,(13, 13),0) #13x13
 
-
+#detecting edges
+sobelHorizontal = cv2.Sobel(gray_image,cv2.CV_64F,1,0,ksize=5) # x dir
+sobelVertical = cv2.Sobel(gray_image,cv2.CV_64F,0,1,ksize=5) # y dir
 
 #num of rows and colums in a plot
-nrows = 2
+nrows = 3
 ncols = 2
 #adding plots
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOrig, 
@@ -31,7 +33,10 @@ plt.subplot(nrows, ncols,3),plt.imshow(img3x3Blur, cmap = 'gray')
 plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,4),plt.imshow(img13x13Blur, cmap = 'gray')
 plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
-
+plt.subplot(nrows, ncols,5),plt.imshow(sobelHorizontal, cmap = 'gray')
+plt.title('Sobel Horizontal'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,6),plt.imshow(sobelVertical, cmap = 'gray')
+plt.title('Sobel Vertical'), plt.xticks([]), plt.yticks([])
 
 #showing plots
 plt.show()
