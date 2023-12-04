027d8cbb2d1d544e886e39e905492aefdbc9d4df
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-04 15:27:53 +0000

    L8-showing images, original and grayscale

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -2,4 +2,18 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('ATU.jpg',)
+imgOrig = cv2.imread('ATU.jpg',)
+cv2.imshow('Original', imgOrig)
+#grayscaling an image
+gray_image = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('Grayscale', gray_image)
+
+nrows = 2
+ncols = 1
+
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOrig, 
+cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.show()
