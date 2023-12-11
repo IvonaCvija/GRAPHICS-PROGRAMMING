eb0dcd99b5dc0573a29406ebcbde8ccf4e156d9c
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-11 11:41:56 +0000

    L9-orb bruteforce working

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -6,9 +6,13 @@ import copy
 #adding images
 #imgOrig = cv2.imread('ATU1.jpg',)
 imgOrig = cv2.imread('cat.jpg',)
+ORBImgOrig1 = cv2.imread('coke.jpg',)
+ORBImgOrig2 = cv2.imread('cokepolarbear.jpg',)
 
 #greyscaling
 gray_image = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+ORBgray_image1 = cv2.cvtColor(ORBImgOrig1, cv2.COLOR_BGR2GRAY) #coke
+ORBgray_image2 = cv2.cvtColor(ORBImgOrig2, cv2.COLOR_BGR2GRAY)  #polar bear with coke
 
 #Harris corners
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
@@ -46,11 +50,43 @@ kp, des = orb.compute(imgOrig, kp)
 imgORB = cv2.drawKeypoints(imgOrig, kp, None, color=(0,255,0), flags=0)
 
 #ORB BRUTE-FORCE
+# find the keypoints and descriptors with ORB
+kp1, des1 = orb.detectAndCompute(ORBgray_image1,None)
+kp2, des2 = orb.detectAndCompute(ORBgray_image2,None)
 
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+# Draw first 10 matches.
+imgORBBruteForce = cv2.drawMatches(ORBgray_image1,kp1,ORBgray_image2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-#showing images
-# cv2.imshow('Original', imgOrig)
-# cv2.waitKey(0)
+#FLANN
+# Initiate SIFT detector
+sift = cv2.SIFT_create()
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+# FLANN parameters
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)   # or pass empty dictionary
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+matches = flann.knnMatch(des1,des2,k=2)
+# Need to draw only good matches, so create a mask
+matchesMask = [[0,0] for i in range(len(matches))]
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.7*n.distance:
+        matchesMask[i]=[1,0]
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = cv2
+                .DrawMatchesFlags_DEFAULT)
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
 #rows and colums for the plot
 nrows = 2
@@ -73,3 +109,6 @@ plt.imshow(cv2.cvtColor(imgORB, cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title(' ORB '), plt.xticks([]), plt.yticks([])
 
 plt.show()
+
+#ORB BruteForce
+plt.imshow(imgORBBruteForce),plt.show()
