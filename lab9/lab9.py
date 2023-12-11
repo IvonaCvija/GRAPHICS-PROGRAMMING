27a68d9443c5f8982c00f4100edec54a96a01919
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-11 11:54:07 +0000

    L9-ORB Flann working

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -4,13 +4,18 @@ from matplotlib import pyplot as plt
 import copy
 
 #adding images
-#imgOrig = cv2.imread('ATU1.jpg',)
-imgOrig = cv2.imread('cat.jpg',)
+imgOrig1 = cv2.imread('ATU1.jpg',)
+imgOrig2 = cv2.imread('ATU2.jpg',)
+
+# imgOriginal = cv2.imread('cat.jpg',)
+# imgOriginal = cv2.imread('temple.jpg',)
+imgOriginal = cv2.imread('malta.jpg',)
+
 ORBImgOrig1 = cv2.imread('coke.jpg',)
 ORBImgOrig2 = cv2.imread('cokepolarbear.jpg',)
 
 #greyscaling
-gray_image = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
 ORBgray_image1 = cv2.cvtColor(ORBImgOrig1, cv2.COLOR_BGR2GRAY) #coke
 ORBgray_image2 = cv2.cvtColor(ORBImgOrig2, cv2.COLOR_BGR2GRAY)  #polar bear with coke
 
@@ -18,7 +23,7 @@ ORBgray_image2 = cv2.cvtColor(ORBImgOrig2, cv2.COLOR_BGR2GRAY)  #polar bear with
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
 
 #deep copy for Harris
-imgHarris = copy.deepcopy(imgOrig)
+imgHarris = copy.deepcopy(imgOriginal)
 
 threshold = 0.15; #number between 0 and 1
 for i in range(len(dst)):
@@ -30,7 +35,7 @@ for i in range(len(dst)):
 corners = cv2.goodFeaturesToTrack(gray_image,88,0.01,10)
 
 #deep copy for Shi Tomasi
-imgShiTomasi = copy.deepcopy(imgOrig)
+imgShiTomasi = copy.deepcopy(imgOriginal)
 
 for i in corners:
     x, y = i[0]  
@@ -41,13 +46,13 @@ for i in corners:
 orb = cv2.ORB_create()
 
 # find the keypoints with ORB
-kp = orb.detect(imgOrig,None)
+kp = orb.detect(imgOriginal,None)
 
 # compute the descriptors with ORB
-kp, des = orb.compute(imgOrig, kp)
+kp, des = orb.compute(imgOriginal, kp)
 
 # draw only keypoints location,not size and orientation
-imgORB = cv2.drawKeypoints(imgOrig, kp, None, color=(0,255,0), flags=0)
+imgORB = cv2.drawKeypoints(imgOriginal, kp, None, color=(0,255,0), flags=0)
 
 #ORB BRUTE-FORCE
 # find the keypoints and descriptors with ORB
@@ -56,27 +61,34 @@ kp2, des2 = orb.detectAndCompute(ORBgray_image2,None)
 
 # create BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
 # Match descriptors.
 matches = bf.match(des1,des2)
+
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
+
 # Draw first 10 matches.
-imgORBBruteForce = cv2.drawMatches(ORBgray_image1,kp1,ORBgray_image2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+imgORBBruteForce = cv2.drawMatches(ORBgray_image1,kp1,ORBgray_image2,kp2,matches[:15],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 #FLANN
 # Initiate SIFT detector
 sift = cv2.SIFT_create()
+
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp1, des1 = sift.detectAndCompute(imgOrig1,None)
+kp2, des2 = sift.detectAndCompute(imgOrig2,None)
+
 # FLANN parameters
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks=50)   # or pass empty dictionary
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 matches = flann.knnMatch(des1,des2,k=2)
+
 # Need to draw only good matches, so create a mask
 matchesMask = [[0,0] for i in range(len(matches))]
+
 # ratio test as per Lowe's paper
 for i,(m,n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
@@ -86,13 +98,14 @@ draw_params = dict(matchColor = (0,255,0),
                    matchesMask = matchesMask,
                    flags = cv2
                 .DrawMatchesFlags_DEFAULT)
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+imgORBFlann = cv2.drawMatchesKnn(imgOrig1,kp1,imgOrig2,kp2,matches,None,**draw_params)
 
+#PLOT
 #rows and colums for the plot
 nrows = 2
 ncols = 3
 #plot
-plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOrig, 
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOriginal, 
 cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title(' Original '), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,2),plt.imshow(cv2.cvtColor(gray_image, 
@@ -110,5 +123,6 @@ plt.title(' ORB '), plt.xticks([]), plt.yticks([])
 
 plt.show()
 
-#ORB BruteForce
+#ORB BruteForce and Flann
 plt.imshow(imgORBBruteForce),plt.show()
+plt.imshow(imgORBFlann),plt.show()
