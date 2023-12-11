076e88d3e91ad436209735afb483db3ce6b95f44
Ivona Cvija <ivca.cvija@gmail.com>
2023-12-11 17:48:31 +0000

    L10-plotting tracks over time

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -16,6 +16,8 @@ cap = cv2.VideoCapture(video_path)
 # Store the track history
 track_history = defaultdict(lambda: [])
 
+#https://github.com/ultralytics/ultralytics/issues/3399
+
 # Loop through the video frames
 while cap.isOpened():
     # Read a frame from the video
@@ -24,24 +26,31 @@ while cap.isOpened():
     if success:
         # Run YOLOv8 tracking on the frame, persisting tracks between frames
         results = model.track(frame, persist=True)
+        for result in results:
+            if result.boxes is None or result.boxes.id is None:
+                continue
+            # Get the boxes and track IDs
+            else:
+                boxes = results[0].boxes.xywh.cpu()
+                track_ids = results[0].boxes.id.int().cpu().tolist()
 
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
+                # Visualize the results on the frame
+                annotated_frame = results[0].plot()
 
-        # Plot the tracks
-        for box, track_id in zip(boxes, track_ids):
-            x, y, w, h = box
-            track = track_history[track_id]
-            track.append((float(x), float(y)))  # x, y center point
-            if len(track) > 30:  # retain 90 tracks for 90 frames
-                track.pop(0)
+                # Plot the tracks
+                for box, track_id in zip(boxes, track_ids):
+                    x, y, w, h = box
+                    track = track_history[track_id]
+                    track.append((float(x), float(y)))  # x, y center point
+                    if len(track) > 30:  # retain 90 tracks for 90 frames
+                        track.pop(0)
 
-            # Draw the tracking lines
-            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+                    # Draw the tracking lines
+                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
 
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+                # Display the annotated frame
+                cv2.imshow("YOLOv8 Tracking", annotated_frame)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
